f232476517ed3eb2c22e1fbb391d99182c830e59
EFernando-73 <epuc2@miumg.edu.gt>
2024-10-17 12:03:59 -0600

    back-36

--- inv/views.py
+++ inv/views.py
@@ -1,13 +1,16 @@
+
+from pyexpat.errors import messages
 from django.forms import BaseModelForm
 from django.http import HttpResponse
+
 from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse_lazy
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .models import Categoria,SubCategoria
-from .forms import CategoriaForm, SubCategoriaForm
+from .models import Categoria,SubCategoria, Marca
+from .forms import CategoriaForm, SubCategoriaForm, MarcaForm
 
 
 class CategoriaView(LoginRequiredMixin, generic.ListView):
@@ -65,3 +68,74 @@ class SubCategoriaNew(LoginRequiredMixin, generic.CreateView):
     def form_valid(self, form):
         form.instance.uc = self.request.user
         return super().form_valid(form)
+
+class SubCategoriaEdit(LoginRequiredMixin, generic.UpdateView):
+    model = SubCategoria
+    template_name="inv/subcategoria_form.html"
+    context_object_name = "obj"
+    form_class = SubCategoriaForm
+    success_url=reverse_lazy("inv:subcategoria_list")
+    login_url = "bases:login"
+
+    def form_valid(self, form):
+        form.instance.um = self.request.user.id
+        return super().form_valid(form)
+
+class SubCategoriaDel(LoginRequiredMixin, generic.DeleteView):
+    model=SubCategoria
+    template_name='inv/catalogos_del.html'
+    context_object_name='obj'
+    success_url=reverse_lazy("inv:subcategoria_list")
+    success_message="Categoría Eliminada Satisfactoriamente"
+
+class MarcaView(LoginRequiredMixin, generic.ListView):
+    model = Marca
+    template_name = "inv/marca_list.html"
+    context_object_name = "obj"
+    login_url = "bases:login"
+
+class MarcaNew(LoginRequiredMixin, generic.CreateView):
+    model=Marca
+    template_name="inv/marca_form.html"
+    context_object_name = 'obj'
+    form_class=MarcaForm
+    success_url= reverse_lazy("inv:marca_list")
+    login_url = "bases:login"
+    
+
+    def form_valid(self, form):
+        form.instance.uc = self.request.user
+        return super().form_valid(form)
+    
+class MarcaEdit(LoginRequiredMixin,
+                   generic.UpdateView):
+    model=Marca
+    template_name="inv/marca_form.html"
+    context_object_name = 'obj'
+    form_class=MarcaForm
+    success_url= reverse_lazy("inv:marca_list")
+    login_url = "bases:login"
+   
+    def form_valid(self, form):
+        form.instance.um = self.request.user.id
+        return super().form_valid(form)
+
+def marca_inactivar(request, id):
+    marca = Marca.objects.filter(pk=id).first()
+    contexto={}
+    template_name="inv/catalogos_del.html"
+
+
+    if not marca:
+        return redirect("inv:marca_list")
+    
+    if request.method=='GET':
+        contexto={'obj':marca}
+    
+    if request.method=='POST':
+        marca.estado=False
+        marca.save()
+        messages.success(request, 'Marca Inactivada')
+        return redirect("inv:marca_list")
+
+    return render(request,template_name,contexto)
